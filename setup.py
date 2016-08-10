8f4f97aa465491c10238341f566f60a599c5d647
Sixem <bentkrisell@gmail.com>
2016-08-10 16:01:21 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(name="imageboard-dl",
+	version="1.0.51",
+	description="Python based image downloader for various imageboards and online image albums.",
+	url="https://github.com/sixem/imageboard-dl",
+	author="sixem",
+	author_email="sixem.mb99@gmail.com",
+	license='MIT',
+	packages=["imageboard-dl"],
+	install_requires=['cfscrape','beautifulsoup4',],
+	scripts=["bin/imageboard-dl"],
+	zip_safe=False)
