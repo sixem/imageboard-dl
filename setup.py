5916b4739d95f9c085fa1c3412a37bba6ddd68d6
Sixem <bentkrisell@gmail.com>
2016-10-13 06:25:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name="imageboard-dl",
-	version="1.0.52",
+	version="1.0.53",
 	description="Python based image downloader for various imageboards and online image albums.",
 	url="https://github.com/sixem/imageboard-dl",
 	author="sixem",
